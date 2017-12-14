1ebc790f275bd34c5aa045c4bda459ae7b2c19dc
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 22:55:31 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -3,13 +3,10 @@ import MySQLdb as mariadb
 import sys, commands
 
 opcion=sys.argv[1]
-base='users'
 usuario=sys.argv[2]
-clave=sys.argv[3]
-uid=sys.argv[4]
-clavecifrada= commands.getoutput("echo "+clave+" | base64")
 
 
+base='users'
 usuadmin='root'
 passadmin='root'
 
@@ -20,7 +17,12 @@ cursor= conectar.cursor()
 
 
 if opcion == '-a':
+	clave=sys.argv[3]
+	uid=sys.argv[4]
+	clavecifrada= commands.getoutput("echo "+clave+" | base64")
+	
 	cursor.execute("INSERT INTO usuario (nombre, clave, id, directorio) VALUES (%s,%s,%s,%s)",(usuario,clavecifrada,uid,directorio))
+
 elif opcion == '-b':
 	cursor.execute("DELETE FROM usuario WHERE nombre = '%s'"%usuario)
 else:
