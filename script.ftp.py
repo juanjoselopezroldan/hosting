b6ed241d7e7ef2883cb1ff92688ca01c2c9e0b77
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 21:20:00 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -1,15 +1,16 @@
 #-*-coding: utf-8 -*-
 import MySQLdb as mariadb
-import sys
+import sys, os
 
 base='usuarios'
 usuario=sys.argv[1]
 clave=sys.argv[2]
+clavecifrada=os.system("echo "+clave+" | base64")
 
 usuadmin='root'
 passadmin='root'
 
 directorio='/srv/ftp/'+usuario
 
-conectar= mariadb.connect("localhost",usuadmin,passadmin,base)
+#conectar= mariadb.connect("localhost",usuadmin,passadmin,base)
 
