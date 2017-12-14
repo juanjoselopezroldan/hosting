ccf9d3d6b13d225b5b85ccca0d59cc0b9f8b251e
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 21:13:58 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -11,4 +11,9 @@ passadmin='root'
 
 directorio='/srv/ftp/'+usuario
 
+os.system("mariadb -u "+usuadmin+" -p"+passadmin" -e 'create user "+usuario+"@'%' identified by "+clave+";'")
+os.system("mariadb -u "+usuadmin+" -p"+passadmin" -e 'create database "+base+";'")
+os.system("mariadb -u "+usuadmin+" -p"+passadmin" -e 'grant all privileges on "+base+".* to "+usuario+"@'%' identified by "+clave+";'")
+
+os.system("mkdir "+directorio)
 
