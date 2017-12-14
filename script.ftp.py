c307415015e8cb1153ec2a50145c1fbde9764840
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 21:39:40 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -3,9 +3,10 @@ import MySQLdb as mariadb
 import sys, commands
 
 opcion=sys.argv[1]
-base='usuarios'
+base='usuario'
 usuario=sys.argv[2]
 clave=sys.argv[3]
+uid=sys.argv[4]
 clavecifrada= commands.getoutput("echo "+clave+" | base64")
 
 
@@ -18,6 +19,5 @@ conectar= mariadb.connect("localhost",usuadmin,passadmin,base)
 cursor= conectar.cursor()
 
 if opcion == '-a':
-
+	cursor.execute("INSERT INTO ftpuser (nombre, clave, id, directorio) VALUES (\'" + nombre + "\',\'" + clavecifrada + "\'," + str(uid) + ",\'" + directorio +"\')")
 elif opcion == '-b':
-	
