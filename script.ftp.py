49fed7e690d6b5c6c77d68cdc6e84d381aa21676
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 22:27:33 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -22,3 +22,8 @@ if opcion == '-a':
 	cursor.execute("INSERT INTO usuario (nombre, clave, id, directorio) VALUES (\'" + usuario + "\',\'" + clavecifrada + "\'," + str(uid) + ",\'" + directorio +"\')")
 elif opcion == '-b':
 	cursor.execute("DELETE FROM usuario WHERE nombre = '%s'"%usuario)
+
+print base
+print usuario
+print clave
+print clavecifrada
