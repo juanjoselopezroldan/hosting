c5edfb1a7c825fb1e85f71a5005187cce5cc7268
juan jose lopez roldan <jjlr1994@gmail.com>
2017-12-14 22:34:19 +0100

    script ftp

--- script.ftp.py
+++ script.ftp.py
@@ -15,11 +15,11 @@ passadmin='root'
 
 directorio='/srv/ftp/'+usuario
 
-conectar= mariadb.connect("localhost",usuadmin,passadmin,base)
+conectar= mariadb.connect("localhost",user=usuadmin,password=passadmin,database=base)
 cursor= conectar.cursor()
 
 if opcion == '-a':
-	cursor.execute("INSERT INTO usuario (nombre, clave, id, directorio) VALUES (\'" + usuario + "\',\'" + clavecifrada + "\'," + str(uid) + ",\'" + directorio +"\')")
+	cursor.execute("INSERT INTO usuario (nombre, clave, id, directorio) VALUES (\'"+usuario+"\',\'"+clavecifrada+"\',"+str(uid)+",\'"+directorio+"\')")
 elif opcion == '-b':
 	cursor.execute("DELETE FROM usuario WHERE nombre = '%s'"%usuario)
 
